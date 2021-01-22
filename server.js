c34fb85019d23d85f103943bc8cc2c1c1d980646
Selena Singleton <ssingle7@gmail.com>
2021-01-21 20:28:58 -0500

    added connection updating the database and the password

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const inquirer = require("inquirer");
-const connection = require('./connection');
+const mysql = require("mysql");
+// const connection = require('./connection');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Savannah20",
+    database: "employee_tracker"
+});
