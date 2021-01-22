fab54854a7eae49f9d8cd8458e29427ec089ed74
Selena Singleton <ssingle7@gmail.com>
2021-01-21 20:51:16 -0500

    added pseudo code to assist with next steps needed for application

--- server.js
+++ server.js
@@ -14,3 +14,10 @@ connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id: ${connection.threadId}`);
 });
+
+// 1.Hard code queries in MySql workbench
+// 2. Bring queries in and add into functions 
+// 3. Test each function individually 
+// 4. Incorporate inquirer.prompt to call these functions  
+// 5. Add additional inquirer prompts to take in user input 
+// 6. Modify the SQL queries to be dynamic 
